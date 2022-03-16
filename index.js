3743010922ee441969c5b100f16f349f6b1873da
Donard97 <donardgolaj@gmail.com>
2022-03-16 22:05:48 +0100

    Create server

---
+++ index.js
@@ -0,0 +1,21 @@
+const app = require("express")();
+const server = require("http").createServer(app);
+const cors = require("cors");
+
+const io = require("socket.io")(server, {
+  cors : {
+    origin: "*",
+    methods: ["GET", "POST"]
+  }
+
+})
+
+app.use(cors());
+
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.send('Server is running.');
+});
+
+server.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
