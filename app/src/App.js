04126d34ca86cc2705a239d9708872d991af0212
Donard97 <donardgolaj@gmail.com>
2022-03-16 23:35:56 +0100

    Create index and app files

---
+++ app/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Typography, AppBar } from '@material-ui/core';
+
+const App = () => {
+  return (
+    <div>
+      <AppBar position="static" color="inherit">
+        <Typography variant="h2" align="center">Donard Meet</Typography>
+      </AppBar>
+    </div>
+  )
+}
+
+export default App
