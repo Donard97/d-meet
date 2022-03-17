e220c78483d1ac34aad62fdb002a91835f9fc0e9
Donard97 <donardgolaj@gmail.com>
2022-03-17 20:23:57 +0100

    Add basic style to app

--- app/src/components/Options.js
+++ app/src/components/Options.js
@@ -1,6 +1,6 @@
-import React from 'react'
+import React from 'react';
 
-const Options = ( {children} ) => {
+const Options = ( { children } ) => {
   return (
     <div>
       Options
--- app/src/index.js
+++ app/src/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
-import ReactDom from 'react-dom';
+import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css'
 
-ReactDom.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
