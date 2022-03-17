56428128f3c5e3aab703dfab13834867702ab0d4
Donard97 <donardgolaj@gmail.com>
2022-03-17 22:00:05 +0100

    Create logic for meet app

--- app/src/index.js
+++ app/src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { ContextProvider } from './SocketContext';
 import './index.css'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ContextProvider>
+      <App />
+    </ContextProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
